80f83ac92c4cd105b1c4a9368574e56d721c3d4b
alexhir <alexandram.hirvonen@gmail.com>
2023-01-07 22:24:12 +0200

    sammakko idle, sit, attack

--- characters/Frog.cs
+++ characters/Frog.cs
@@ -4,151 +4,151 @@ using System.Collections.Generic;
 
 public partial class Frog : StaticBody2D {
 
-    enum State {
-        ReadyToAttack,
-        AttackOnCooldown,
-        AttackCharging,
-        Idle
-    };
+	enum State {
+		ReadyToAttack,
+		AttackOnCooldown,
+		AttackCharging,
+		Idle
+	};
 
-    private State state = State.Idle;
+	private State state = State.Idle;
 
-    // targets within frog's attack radius
-    private List<Fly> tracking_targets = new();
-
-
-    // frog sees a fly -> he starts winding up attack
-    private Timer attack_charge;
-
-    // frog has caught a fly -> he's chewing it and can't attack for another few seconds
-    private Timer cooldown;
-
-    // after winding up an attack, there is a small window of time to attack
-    private Timer attack_window;
-
-    private AnimatedSprite2D animation;
+	// targets within frog's attack radius
+	private List<Fly> tracking_targets = new();
 
 
-    public override void _Ready() {
-        animation = GetNode<AnimatedSprite2D>("Animation");
-        animation.Animation = "default";
-        animation.Playing = true;
+	// frog sees a fly -> he starts winding up attack
+	private Timer attack_charge;
 
-        var attack_area = GetNode<Area2D>("AttackArea");
-        attack_area.AreaEntered += on_area_entered;
-        attack_area.AreaExited += on_area_exited;
+	// frog has caught a fly -> he's chewing it and can't attack for another few seconds
+	private Timer cooldown;
 
-        attack_charge = GetNode<Timer>("Charge");
-        attack_charge.Timeout += attack_charged;
+	// after winding up an attack, there is a small window of time to attack
+	private Timer attack_window;
 
-        cooldown = GetNode<Timer>("Cooldown");
-        cooldown.Timeout += cooldown_done;
+	private AnimatedSprite2D animation;
 
-        attack_window = GetNode<Timer>("AttackWindow");
-        attack_window.Timeout += missed_window_to_attack;
-    }
 
-    public override void _Process(double delta) {
-        var enemies_nearby = this.tracking_targets.Count > 0;
+	public override void _Ready() {
+		animation = GetNode<AnimatedSprite2D>("Animation");
+		animation.Animation = "default";
+		animation.Playing = true;
 
-        if (this.state == State.Idle) {
-            if (enemies_nearby) {
-                start_attack_charge();
-                return;
-            }
-        }
+		var attack_area = GetNode<Area2D>("AttackArea");
+		attack_area.AreaEntered += on_area_entered;
+		attack_area.AreaExited += on_area_exited;
 
-        if (this.state == State.ReadyToAttack) {
-            if (enemies_nearby) {
-                attack();
-                return;
-            }
-        }
+		attack_charge = GetNode<Timer>("Charge");
+		attack_charge.Timeout += attack_charged;
 
-    }
+		cooldown = GetNode<Timer>("Cooldown");
+		cooldown.Timeout += cooldown_done;
 
-    private void attack() {
-        GD.Print("Slap");
-        destroy_nearest_fly();
-        // fly.destroy()
-        cooldown.Start();
-        animation.Animation = "chewing";
-        this.state = State.AttackOnCooldown;
-    }
+		attack_window = GetNode<Timer>("AttackWindow");
+		attack_window.Timeout += missed_window_to_attack;
+	}
 
-    private void destroy_nearest_fly() {
-        if (tracking_targets.Count == 0) {
-            // how did we even get here?
-            return;
-        }
+	public override void _Process(double delta) {
+		var enemies_nearby = this.tracking_targets.Count > 0;
 
-        var target = tracking_targets[0];
+		if (this.state == State.Idle) {
+			if (enemies_nearby) {
+				start_attack_charge();
+				return;
+			}
+		}
 
-        if (tracking_targets.Count > 1) {
-            var shortest_distance = dist2_to_me(target);
+		if (this.state == State.ReadyToAttack) {
+			if (enemies_nearby) {
+				attack();
+				return;
+			}
+		}
 
-            foreach (var fly in tracking_targets) {
-                var dist = dist2_to_me(fly);
-                if (dist < shortest_distance) {
-                    shortest_distance = dist;
-                    target = fly;
-                }
-            }
-        }
+	}
 
-        tracking_targets.Remove(target);
-        target.QueueFree();
+	private void attack() {
+		GD.Print("Slap");
+		destroy_nearest_fly();
+		// fly.destroy()
+		cooldown.Start();
+		animation.Animation = "chewing";
+		this.state = State.AttackOnCooldown;
+	}
 
-    }
+	private void destroy_nearest_fly() {
+		if (tracking_targets.Count == 0) {
+			// how did we even get here?
+			return;
+		}
 
-    private float dist2_to_me(Fly other) {
-        return this.Position.DistanceSquaredTo(other.Position);
-    }
+		var target = tracking_targets[0];
 
-    private void start_attack_charge() {
-        GD.Print("Charging attack");
-        animation.Animation = "prepared";
-        state = State.AttackCharging;
-        attack_charge.Start();
-    }
+		if (tracking_targets.Count > 1) {
+			var shortest_distance = dist2_to_me(target);
 
-    public void on_area_entered(Area2D area) {
-        if (area.GetType() != typeof(Fly)) {
-            return;
-        }
+			foreach (var fly in tracking_targets) {
+				var dist = dist2_to_me(fly);
+				if (dist < shortest_distance) {
+					shortest_distance = dist;
+					target = fly;
+				}
+			}
+		}
 
-        var fly = (Fly)area;
-        tracking_targets.Add(fly);
-    }
+		tracking_targets.Remove(target);
+		target.QueueFree();
 
-    public void on_area_exited(Area2D area) {
-        if (area.GetType() != typeof(Fly)) {
-            return;
-        }
+	}
 
-        var fly = (Fly)area;
-        tracking_targets.Remove(fly);
-    }
+	private float dist2_to_me(Fly other) {
+		return this.Position.DistanceSquaredTo(other.Position);
+	}
 
-    private void attack_charged() {
-        GD.Print("Ready to attack");
+	private void start_attack_charge() {
+		GD.Print("Charging attack");
+		animation.Animation = "prepared";
+		state = State.AttackCharging;
+		attack_charge.Start();
+	}
 
-        this.state = State.ReadyToAttack;
-        animation.Animation = "prepared";
-        attack_window.Start();
-    }
+	public void on_area_entered(Area2D area) {
+		if (area.GetType() != typeof(Fly)) {
+			return;
+		}
 
-    private void cooldown_done() {
-        GD.Print("Cooldown done");
+		var fly = (Fly)area;
+		tracking_targets.Add(fly);
+	}
 
-        state = State.Idle;
-        animation.Animation = "default";
-    }
+	public void on_area_exited(Area2D area) {
+		if (area.GetType() != typeof(Fly)) {
+			return;
+		}
 
-    private void missed_window_to_attack() {
-        GD.Print("Missed attack window");
+		var fly = (Fly)area;
+		tracking_targets.Remove(fly);
+	}
 
-        state = State.Idle;
-        animation.Animation = "default";
-    }
+	private void attack_charged() {
+		GD.Print("Ready to attack");
+
+		this.state = State.ReadyToAttack;
+		animation.Animation = "prepared";
+		attack_window.Start();
+	}
+
+	private void cooldown_done() {
+		GD.Print("Cooldown done");
+
+		state = State.Idle;
+		animation.Animation = "default";
+	}
+
+	private void missed_window_to_attack() {
+		GD.Print("Missed attack window");
+
+		state = State.Idle;
+		animation.Animation = "default";
+	}
 }
